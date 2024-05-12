48e85994a4834ee57179f7899149c363317a417b
JOC6BK_HFT_2023242 <kalap20030307@gmail.com>
2024-05-12 15:54:30 +0200

    StatController created

---
+++ JOC6BK_HFT_2023242.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JOC6BK_HFT_2023242.Endpoint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        // GET: api/<StatController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<StatController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<StatController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<StatController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<StatController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
